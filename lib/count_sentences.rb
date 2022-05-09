be6ed402bf161aaf9192a1f980635407e66319da
Brian <b.perez3237@gmail.com>
2022-05-09 19:00:26 -0400

    commit tree d4e070e20c9dbc3cced24627a27d36885c222c9c

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self[-1] == "?"
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    number_of_words = self.split.sum do |word|
+      if word.exclamation? || word.question? || word.sentence?
+        1
+      else 
+        0
+      end
+    end
+    number_of_words
   end
 end
+
